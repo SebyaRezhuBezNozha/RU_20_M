05d14b3295b5ab7a97bb4faa77a71002e1c69ee0
SebyaRezhuBezNozha <sebya-rezhu-bez-nozha-dostabl@yandex.ru>
2026-02-01 06:00:00 +0300

    Add files via upload

--- js/quizer.js
+++ js/quizer.js
@@ -557,6 +557,7 @@ function map_songs(){
 	$('#mirror').hide();
 	$('#map').hide();
 	$('#package_content').hide();
+	$('#sec_15_hist').show();
 	$('#mapping_content').show();
 	toggleLearn();
 	for(var j=0; j < music.length; j++){
@@ -904,6 +905,15 @@ function back_to_browser(){
 function back_to_current_pack(){
 	back = back_to_browser;
 	$('#mapping_content').hide();
+	$('#sec_15_hist').hide();
+	song_stop();
 	$('#map').show();
 	package_num(pack_num);
+}
+
+function song_stop() {
+	if(audio){
+		audio.pause();
+		audio = null;
+	}
 }
